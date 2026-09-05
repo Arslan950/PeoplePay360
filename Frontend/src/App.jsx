492f7685efdabe3b15b6c827b6f9e2d9fdedb310
fy6473 <ankurshukla@gmail.com>
2026-09-05 10:45:34 +0530

    intial commit, set up basic react

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-white">
+        PeoplePay360
+      </h1>
+    </div>
+  )
+}
+
+
+export default App
